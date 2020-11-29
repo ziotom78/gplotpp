9a9d92a875745d409232d637e7a481fcb3494abc
Maurizio Tomasi <ziotom78@gmail.com>
2020-11-29 13:52:45 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# gplot++
+
+A header-only C++ interface to Gnuplot.
+
+This repository contains the file `gplot++.h`, which provides a way for C++ programs to connect to a Gnuplot instance to produce plots.
+
+A few features of this library are the following:
+
+- Header-only library: very easy to install
+- Plot `std::vector` variables
+- Multiple series in the same plot
+- Multiple plots (via `Gnuplot::multiplot`)
+- Logarithmic axes (via `Gnuplot::set_logscale`)
+- Histograms (via `Gnuplot::histogram`)
+- Custom ranges (via `Gnuplot::set_xrange` and `Gnuplot::set_yrange`)
+- Possibility to save the plots in PNG and PDF files
+
+## Examples
+
+Here is the output of one of the examples:
+
+![](./images/complex.png)
+
+The source code of the example is in file `example-complex.cpp`:
+
+```c++
+#include "gplot++.h"
+
+int main(void) {
+  Gnuplot plt{};
+  std::vector<double> x{1, 2, 3, 4, 5}, y{5, 2, 4, 1, 3};
+
+  // Save the plot into a PNG file with the desired size (in pixels)
+  plt.redirect_to_png("complex.png", "800,600");
+
+  /* Create two plots with the following layout (2 rows, 1 column):
+   *
+   * +------------------------+
+   * |                        |
+   * |        Plot #1         |
+   * |                        |
+   * +------------------------+
+   * |                        |
+   * |        Plot #2         |
+   * |                        |
+   * +------------------------+
+   */
+  plt.multiplot(2, 1, "Title");
+
+  // Plot #1
+  plt.set_xlabel("X axis");
+  plt.set_ylabel("Y axis");
+  plt.plot(x, y, "x-y plot");
+  plt.plot(y, x, "y-x plot", Gnuplot::LineStyle::LINESPOINTS);
+  plt.show(); // Always call "show"!
+
+  // Plot #2
+  plt.set_xlabel("Value");
+  plt.set_ylabel("Number of counts");
+  plt.histogram(y, 2, "Histogram");
+  plt.set_xrange(-1, 7);
+  plt.set_yrange(0, 5);
+  plt.show(); // Always call "show"!
+}
+```
+
+## Low-level interface
+
+You can pass commands to Gnuplot using the method `Gnuplot::sendcommand`:
+
+```c++
+Gnuplot plt{};
+
+plt.sendcommand("set xlabel 'X axis'");
+plt.sendcommand("plot sin(x)");
+```
+
+## Similar libraries
+
+There are several other libraries like gplot++ around. These are the
+ones I referenced while developing my own's:
+
+-   [gnuplot-iostream](https://github.com/dstahlke/gnuplot-iostream):
+    it has much more features than gplot++, but it is slightly more
+    difficult to install and use.
+-   [gnuplot-cpp](https://github.com/martinruenz/gnuplot-cpp): another
+    header-only library. It lacks support for high-level stuff like
+    `std::vector` plotting.
---
+++ gplot++.h
@@ -0,0 +1,352 @@
+#pragma once
+
+/* gplot++
+ *
+ * A header-only C++ interface to Gnuplot.
+ *
+ * Copyright 2020 Maurizio Tomasi
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Version history
+ *
+ * - 0.1.0 (2020/11/29): first release
+ *
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// The "sleep" function is non-standard
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+/**
+ * High-level interface to the Gnuplot executable
+ *
+ * This class establishes a connection with a new Gnuplot instance and
+ * sends commands to it through a pipe. It is able to directly plot
+ * vectors of numbers by saving them in temporary files.
+ */
+class Gnuplot {
+private:
+  // Create a name for a temporary file and return it
+  std::string tmp_file_name() {
+    // Calling "tmpnam" makes GCC emit a warning, but there is no
+    // portable way to create a temporary file name in C++
+    std::string filename{std::tmpnam(nullptr)};
+    files_to_delete.push_back(filename);
+
+    return filename;
+  }
+
+  std::string escape_quotes(const std::string &s) {
+    std::string result{};
+
+    for (char c : s) {
+      if (c != '\'')
+        result.push_back(c);
+      else
+        result += "''";
+    }
+
+    return result;
+  }
+
+public:
+  enum class LineStyle {
+    DOTS,
+    LINES,
+    POINTS,
+    LINESPOINTS,
+    STEPS,
+    BOXES,
+  };
+
+  enum class AxisScale {
+    LINEAR,
+    LOGX,
+    LOGY,
+    LOGXY,
+  };
+
+  Gnuplot(const char *executable_name = "gnuplot")
+      : connection{}, series{}, files_to_delete{} {
+    std::stringstream os;
+    // The --persist flag lets Gnuplot keep running after the C++
+    // program has completed its execution
+    os << executable_name << " "
+       << "--persist";
+    connection = popen(os.str().c_str(), "w");
+
+    set_xrange();
+    set_yrange();
+
+    // See
+    // https://stackoverflow.com/questions/28152719/how-to-make-gnuplot-use-the-unicode-minus-sign-for-negative-numbers
+    sendcommand("set encoding utf8\n");
+    sendcommand("set minussign");
+  }
+
+  ~Gnuplot() {
+    // Bye bye, Gnuplot!
+    if (connection) {
+      pclose(connection);
+      connection = nullptr;
+    }
+
+    // Let some time pass before removing the files, so that Gnuplot
+    // can finish displaying the last plot.
+    sleep(1);
+
+    // Now remove the data files
+    for (const auto &fname : files_to_delete) {
+      std::remove(fname.c_str());
+    }
+  }
+
+  /* This is the most low-level method in the Gnuplot class! It
+         returns `true` if the send command was successful, `false`
+         otherwise. */
+  bool sendcommand(const char *str) {
+    if (!ok())
+      return false;
+
+    fputs(str, connection);
+    fputc('\n', connection);
+    return true;
+  }
+
+  bool sendcommand(const std::string &str) { return sendcommand(str.c_str()); }
+  bool sendcommand(const std::stringstream &stream) {
+    return sendcommand(stream.str());
+  }
+
+  bool ok() { return connection != nullptr; }
+
+  /* Save the plot to a PNG file instead of displaying a window */
+  bool redirect_to_png(const std::string &filename,
+                       const std::string &size = "800,600") {
+    std::stringstream os;
+
+    os << "set terminal pngcairo color enhanced size " << size << "\n"
+       << "set output '" << filename << "'\n";
+    return sendcommand(os);
+  }
+
+  /* Save the plot to a PDF file instead of displaying a window */
+  bool redirect_to_pdf(const std::string &filename,
+                       std::string size = "16cm,12cm") {
+    std::stringstream os;
+
+    os << "set terminal pdfcairo color enhanced size " << size << "\n"
+       << "set output '" << filename << "'\n";
+    return sendcommand(os);
+  }
+
+  /* Set the label on the X axis */
+  bool set_xlabel(const std::string &label) {
+    std::stringstream os;
+    os << "set xlabel '" << escape_quotes(label) << "'";
+    return sendcommand(os);
+  }
+
+  /* Set the label on the Y axis */
+  bool set_ylabel(const std::string &label) {
+    std::stringstream os;
+    os << "set ylabel '" << escape_quotes(label) << "'";
+    return sendcommand(os);
+  }
+
+  /* Set the minimum and maximum value to be displayed along the X axis */
+  void set_xrange(double min = NAN, double max = NAN) {
+    xrange = format_range(min, max);
+  }
+
+  /* Set the minimum and maximum value to be displayed along the X axis */
+  void set_yrange(double min = NAN, double max = NAN) {
+    yrange = format_range(min, max);
+  }
+
+  bool set_logscale(AxisScale scale) {
+    switch (scale) {
+    case AxisScale::LOGX:
+      return sendcommand("set logscale x");
+    case AxisScale::LOGY:
+      return sendcommand("set logscale y");
+    case AxisScale::LOGXY:
+      return sendcommand("set logscale xy");
+    default:
+      return sendcommand("unset logscale");
+    }
+  }
+
+  template <typename T>
+  void plot(const std::vector<T> &y, const std::string &label = "",
+            LineStyle style = LineStyle::LINES) {
+    if (y.empty())
+      return;
+
+    std::string filename{tmp_file_name()};
+    std::ofstream of{filename};
+    assert(of.good());
+    for (const auto &val : y) {
+      of << y << "\n";
+    }
+
+    series.push_back(GnuplotSeries{filename, style, label, "0:1"});
+  }
+
+  template <typename T>
+  void plot(const std::vector<T> &x, const std::vector<T> &y,
+            const std::string &label = "", LineStyle style = LineStyle::LINES) {
+    assert(x.size() == y.size());
+
+    if (x.empty())
+      return;
+
+    std::string filename{tmp_file_name()};
+    std::ofstream of{filename};
+    assert(of.good());
+    for (size_t i{}; i < x.size(); ++i) {
+      of << x[i] << " " << y[i] << "\n";
+    }
+
+    series.push_back(GnuplotSeries{filename, style, label, "1:2"});
+  }
+
+  template <typename T>
+  void histogram(const std::vector<T> &values, size_t nbins,
+                 const std::string &label = "",
+                 LineStyle style = LineStyle::BOXES) {
+    assert(nbins > 0);
+
+    if (values.empty())
+      return;
+
+    double min = *std::min_element(values.begin(), values.end());
+    double max = *std::max_element(values.begin(), values.end());
+    double binwidth = (max - min) / nbins;
+
+    std::vector<size_t> bins(nbins);
+    for (const auto &val : values) {
+      int index = (val - min) / binwidth;
+      if (index >= int(nbins))
+        --index;
+
+      bins.at(index)++;
+    }
+
+    std::string filename{tmp_file_name()};
+    std::ofstream of{filename};
+    assert(of.good());
+    for (size_t i{}; i < nbins; ++i) {
+      of << min + binwidth * (i + 0.5) << " " << bins[i] << "\n";
+    }
+
+    series.push_back(GnuplotSeries{filename, style, label, "1:2"});
+  }
+
+  bool multiplot(int nrows, int ncols, const std::string &title = "") {
+    std::stringstream os;
+    os << "set multiplot layout " << nrows << ", " << ncols << " title '"
+       << escape_quotes(title) << "'\n";
+    return sendcommand(os);
+  }
+
+  bool show(bool call_reset = true) {
+    std::stringstream os;
+    os << "set style fill solid 0.5\n";
+
+    os << "plot " << xrange << " " << yrange << " ";
+    for (size_t i{}; i < series.size(); ++i) {
+      const GnuplotSeries &s = series.at(i);
+      os << "'" << s.filename << "' using " << s.column_range << " with "
+         << style_to_str(s.line_style) << " title '" << escape_quotes(s.title)
+         << "'";
+
+      if (i + 1 < series.size())
+        os << ", ";
+    }
+
+    bool result = sendcommand(os);
+    if (result && call_reset)
+      reset();
+
+    return result;
+  }
+
+  void reset() {
+    series.clear();
+    set_xrange();
+    set_yrange();
+  }
+
+private:
+  struct GnuplotSeries {
+    std::string filename;
+    LineStyle line_style;
+    std::string title;
+    std::string column_range;
+  };
+
+  std::string style_to_str(LineStyle style) {
+    switch (style) {
+    case LineStyle::DOTS:
+      return "dots";
+    case LineStyle::POINTS:
+      return "points";
+    case LineStyle::LINESPOINTS:
+      return "linespoints";
+    case LineStyle::STEPS:
+      return "steps";
+    case LineStyle::BOXES:
+      return "boxes";
+    default:
+      return "lines";
+    }
+  }
+
+  std::string format_range(double min = NAN, double max = NAN) {
+    if (std::isnan(min) || std::isnan(max))
+      return "[]";
+
+    std::stringstream os;
+    os << "[" << min << ":" << max << "]";
+
+    return os.str();
+  }
+
+  FILE *connection;
+  std::vector<GnuplotSeries> series;
+  std::vector<std::string> files_to_delete;
+  std::string xrange;
+  std::string yrange;
+};

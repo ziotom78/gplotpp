67fdac205875cd41e5324fdeb5308753f0dcfc14
Maurizio Tomasi <ziotom78@gmail.com>
2021-12-02 12:44:53 +0100

    Remove all tests but "complex" and use SVG instead of dumb terminal

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -15,10 +15,10 @@ DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_END
 
 using namespace std;
 
-string read_file(const char *file_name) {
+string read_file(const string & file_name) {
     INFO("Reading text file ", file_name);
 
-    FILE *fin{fopen(file_name, "rt")};
+    FILE *fin{fopen(file_name.c_str(), "rt")};
     CHECK(fin != nullptr);
 
     string result;
@@ -38,52 +38,13 @@ string read_file(const char *file_name) {
 // writing stuff on the "dumb" terminal
 void wait() { std::this_thread::sleep_for(std::chrono::milliseconds(1000)); }
 
-TEST_CASE("simple plot") {
-    {
-        Gnuplot plt{};
-
-        plt.sendcommand("set terminal dumb 40 20");
-        plt.sendcommand("set output 'simple-plot.txt'");
-
-        vector<double> x{1, 2, 3, 4, 5};
-        vector<double> y{5, 4, 3, 2, 1};
-
-        plt.plot(x, y);
-        plt.show();
-    }  // Call Gnuplot::~Gnuplot() for plt and save the file
-
-    wait();
-
-    string file_contents{read_file("simple-plot.txt")};
-    CHECK(file_contents ==
-          "                                        "
-          "      +-----------------------------+   "
-          "    5 |**  +  +   +  +  +   +  +  ++|   "
-          "      |  **                         |   "
-          "  4.5 |-+  **                     +-|   "
-          "    4 |-+    **                   +-|   "
-          "      |        **                   |   "
-          "  3.5 |-+        **               +-|   "
-          "      |            **               |   "
-          "    3 |-+            *            +-|   "
-          "      |               **            |   "
-          "  2.5 |-+               **        +-|   "
-          "      |                   **        |   "
-          "    2 |-+                   **    +-|   "
-          "  1.5 |-+                     **  +-|   "
-          "      |                         **  |   "
-          "    1 |++  +  +   +  +  +   +  +  **|   "
-          "      +-----------------------------+   "
-          "       1  1.5 2  2.5 3 3.5  4 4.5  5    "
-          "                                        ");
-}
-
 TEST_CASE("complex") {
+  const string file_name{"complex.svg"};
+  
     {
         Gnuplot plt{};
 
-        plt.sendcommand("set terminal dumb 40 20");
-        plt.sendcommand("set output 'complex.txt'");
+		plt.redirect_to_svg(file_name);
 
         vector<double> x{1, 2, 3, 4, 5};
         vector<double> y{5, 4, 3, 2, 1};
@@ -97,66 +58,10 @@ TEST_CASE("complex") {
 
     wait();
 
-    string file_contents{read_file("complex.txt")};
-    CHECK(file_contents ==
-          "                                        "
-          "         +--------------------------+   "
-          "       5 |C+ +  +  +   +  +  +  + +D|   "
-          "     4.5 |-+      Series #1    C  +-|   "
-          "         |        Series #2    D    |   "
-          "       4 |-+    C            D    +-|   "
-          "         |                          |   "
-          "     3.5 |-+                      +-|   "
-          "       3 |-+           D          +-|   "
-          "         |                          |   "
-          "     2.5 |-+                      +-|   "
-          "         |                          |   "
-          "       2 |-+    D            C    +-|   "
-          "         |                          |   "
-          "     1.5 |-+                      +-|   "
-          "       1 |D+ +  +  +   +  +  +  + +C|   "
-          "         +--------------------------+   "
-          "          1 1.5 2 2.5  3 3.5 4 4.5 5    "
-          "                   X axis               "
-          "                                        ");
-}
+    string file_contents{read_file(file_name)};
 
-TEST_CASE("histogram") {
-    {
-        Gnuplot plt{};
-
-        plt.sendcommand("set terminal dumb 40 20");
-        plt.sendcommand("set output 'histogram.txt'");
-
-        vector<double> samples{1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3,
-                               3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6};
-
-        plt.histogram(samples, 3);
-        plt.show();
-    }  // Call Gnuplot::~Gnuplot() for plt and save the file
-
-    wait();
-
-    string file_contents{read_file("histogram.txt")};
-    CHECK(file_contents ==
-          "                                        "
-          "       +----------------------------+   "
-          "    12 |-++   +   +   +  +  ********|   "
-          "       |                    *       |   "
-          "  11.5 |-+                  *     +-|   "
-          "       |                    *       |   "
-          "       |                    *       |   "
-          "    11 |********            *     +-|   "
-          "       |       *            *       |   "
-          "  10.5 |-+     *            *     +-|   "
-          "       |       *            *       |   "
-          "    10 |-+     *            *     +-|   "
-          "       |       *            *       |   "
-          "       |       *            *       |   "
-          "   9.5 |-+     *            *     +-|   "
-          "       |       *            *       |   "
-          "     9 |-++   +**************+   ++-|   "
-          "       +----------------------------+   "
-          "          2  2.5  3  3.5 4  4.5  5      "
-          "                                        ");
+	CHECK(file_contents.find("Series #1") != string::npos);
+	CHECK(file_contents.find("Series #2") != string::npos);
+	CHECK(file_contents.find("X axis") != string::npos);
+	CHECK(file_contents.find("Y axis") != string::npos);
 }
